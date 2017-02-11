0c3a6095cab6639601fa2b2bd7428715a6b987fd
Antharam Shiva Kumar <antharamshivakumar@gmail.com>
2017-02-11 15:58:58 +0530

    Add files via upload

---
+++ assignmet 4.2.ts
@@ -0,0 +1,38 @@
+/*
+
+Create a Class Animal, which is Base class should have name, distance covered by and behavior of that animal:
+Class Animal {name: string; move(distance) {}; makeSound() {} ...}
+Then create 4 more class which will extend Animal class with its property and behavior. For example:
+Class Tiger extends Animal {...}
+
+*/
+
+class Animal {
+    name: string;
+    constructor(theName: string) { this.name = theName; }
+    move(distanceInMeters: number = 0) {
+        console.log(`${this.name} moved ${distanceInMeters}m.`);
+    }
+}
+
+class Snake extends Animal {
+    constructor(name: string) { super(name); }
+    move(distanceInMeters = 5) {
+        console.log("Slithering...");
+        super.move(distanceInMeters);
+    }
+}
+
+class Horse extends Animal {
+    constructor(name: string) { super(name); }
+    move(distanceInMeters = 45) {
+        console.log("Galloping...");
+        super.move(distanceInMeters);
+    }
+}
+
+let sam = new Snake("Sammy the Python");
+let tom: Animal = new Horse("Tommy the Palomino");
+
+sam.move();
+tom.move(34);
